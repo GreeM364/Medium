6973056b43461b735784688303f4f2da2a767263
Denis <denisrubel40@gmail.com>
2023-09-10 21:48:10 +0300

    Added a registration module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {isDevMode, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AuthModule} from './auth/auth.module'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {HttpClientModule} from '@angular/common/http'
+import {EffectsModule} from '@ngrx/effects'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
